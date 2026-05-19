afafc3404e7ca97b75108680261f7b4dab850369
bartur8195 <bartur8195@gmail.com>
2026-05-19 16:52:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# 4
+def matn_teskarisi():
+    soz = "Python"
+    teskari = soz[::-1]
+    for _ in range(3):
+        print(teskari)
+
+# 5
+def murakkab_hisob():
+    a, b, c = 10, 20, 30
+    yigindi = a + b + c
+    kopaytma = a * b * c
+
+    print(f"Sonlar: {a}, {b}, {c}")
+    print(f"Yig‘indi: {yigindi}")
+    print(f"Ko‘paytma: {kopaytma}")
